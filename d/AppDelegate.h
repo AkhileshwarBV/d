a8e9d6590dbd7d37b6d543726230d693ccb1937f
Delta Technology <deltatechnology@Deltas-iMac.local>
2014-11-10 21:22:57 +0530

    Initial Commit

---
+++ d/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  d
+//
+//  Created by Delta Technology on 11/10/14.
+//  Copyright (c) 2014 Delta Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ d/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  d
+//
+//  Created by Delta Technology on 11/10/14.
+//  Copyright (c) 2014 Delta Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
